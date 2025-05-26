38a12926ca4f7c1c0405ec3b183661efa88c3bc8
mfrszpiotro <marcel.piotrowski5543@gmail.com>
2025-05-26 17:13:42 +0200

    feat: implement Strategy design pattern and extend the User definition

--- redeems/redeems.py
+++ redeems/redeems.py
@@ -1,32 +1,124 @@
-from dataclasses import dataclass
 from decimal import Decimal
 import logging
-from typing import NewType
+from typing import NewType, Dict, Optional
+from abc import abstractmethod, ABC
+from enum import Enum
 
 
 logger = logging.getLogger(__name__)
 
 
+UserId = NewType("UserId", str)
 AccountNo = NewType("AccountNo", str)
+Points = NewType("Points", Decimal)
 
 
-@dataclass
-class User:
-    id: str
-    account_no: AccountNo
+class CountryCode(str, Enum):
+    FR = "FR"
+    DE = "DE"
 
 
 class PaymentGateway:
-    def transfer(self, account_no: AccountNo, amount: Decimal) -> None:
-        logger.info(f"Successfully transferred {amount} to {account_no}")
+    def transfer(self, account_no: AccountNo, money_amount: Decimal) -> None:
+        logger.info(f"Successfully transferred {money_amount} to {account_no}")
+
+
+class RedemptionStrategy(ABC):
+    required_points: Points
+
+    def redeem_points(
+        self,
+        points: Points,
+        payment_gateway: Optional[PaymentGateway],
+        account_no: Optional[AccountNo],
+    ):
+        if self._check_sufficient_points(points):
+            self._process_points(points, payment_gateway, account_no)
+
+    def _check_sufficient_points(self, points: Points) -> bool:
+        predicate = points >= self.required_points
+        if not predicate:
+            logger.info(
+                f"Not enough points, got {points} out of {self.required_points}"
+            )
+        return predicate
+
+    @abstractmethod
+    def _process_points(
+        self,
+        points: Points,
+        payment_gateway: Optional[PaymentGateway],
+        account_no: Optional[AccountNo],
+    ): ...
+
+
+class BankTransfer(RedemptionStrategy):
+    class MissingPaymentDetailsException(Exception):
+        pass
+
+    def __init__(self, required_points: Points, award_amount: Decimal):
+        self.required_points = required_points
+        self.award_amount = award_amount
+
+    def _process_points(
+        self,
+        points: Points,
+        payment_gateway: Optional[PaymentGateway],
+        account_no: Optional[AccountNo],
+    ):
+        if payment_gateway and account_no:
+            payment_gateway.transfer(account_no, self.award_amount)
+            print(
+                f"We've sent a transfer to your account "
+                f"for {self.award_amount}. Don't spend it all at once!"
+            )
+        else:
+            raise self.MissingPaymentDetailsException()
+
+
+class GiftCard(RedemptionStrategy):
+    def __init__(self, required_points: Points):
+        self.required_points = required_points
+
+    def _process_points(
+        self,
+        points: Points,
+        payment_gateway: Optional[PaymentGateway],
+        account_no: Optional[AccountNo],
+    ):
+        print(
+            f"Here is your gift card for redeeming your {points} points. Use it wisely!"
+        )
+
+
+class User:
+    def __init__(
+        self,
+        id: UserId,
+        points: Points,
+        account_no: AccountNo,
+        country_code: CountryCode,
+    ) -> None:
+        self.id = id
+        self.points = points
+        self.account_no = account_no
+        self.country_code = country_code
+        self.redemption_strategy = COUNTRY_STRATEGY_CONFIG[country_code]
+        pass
 
 
 class RedemptionService:
-    def __init__(self, payment_gateway) -> None:
+    def __init__(self, payment_gateway: PaymentGateway) -> None:
         self._payment_gateway = payment_gateway
 
-    def redeem(self, user: User, amount: Decimal) -> None:
-        # Assumption: user will redeem `amount` of money.
-        # We're not supporting other types of awards.
-        self._payment_gateway(user.account_no, amount)
-        logger.info(f"Redeemed {amount}, for a {user}")
+    def redeem(self, user: User) -> None:
+        user.redemption_strategy.redeem_points(
+            user.points, self._payment_gateway, user.account_no
+        )
+        logger.info(f"Redeemed {user.points} points, for user {user.id}")
+
+
+COUNTRY_STRATEGY_CONFIG: Dict[CountryCode, RedemptionStrategy] = {
+    CountryCode.DE: BankTransfer(Points(Decimal(1000)), Decimal(50)),
+    CountryCode.FR: GiftCard(Points(Decimal(2000))),
+}
--- redeems/tests.py
+++ redeems/tests.py
@@ -2,68 +2,83 @@ import random
 import string
 from decimal import Decimal
 from unittest import TestCase, mock
-import unittest
 
-from .redeems import AccountNo, PaymentGateway, RedemptionService, User, logger
+from .redeems import (
+    AccountNo,
+    Points,
+    PaymentGateway,
+    RedemptionService,
+    User,
+    UserId,
+    logger,
+    CountryCode,
+    BankTransfer,
+)
 
 
 class UserFactory:
     @staticmethod
-    def create() -> User:
-        user_id = "".join(random.choices((string.ascii_letters + string.digits), k=5))
-        account_no = "".join(random.choices(string.digits, k=20))
-        return User(id=user_id, account_no=AccountNo(account_no))
+    def create(country_code: CountryCode) -> User:
+        user_id = UserId(
+            "".join(random.choices((string.ascii_letters + string.digits), k=5))
+        )
+        account_no = AccountNo("".join(random.choices(string.digits, k=20)))
+        return User(user_id, Points(Decimal(0)), account_no, country_code)
 
 
 class TestRedemptionService(TestCase):
     def setUp(self) -> None:
-        self.payment_gateway = mock.create_autospec(PaymentGateway)
+        self.payment_gateway = mock.Mock(spec=["transfer"])
         self.redemption_service = RedemptionService(
             payment_gateway=self.payment_gateway,
         )
-        self.exemplary_user = UserFactory.create()
-        self.exemplary_sufficient_points = Decimal(1000)
-        self.exemplary_insufficient_points = self.exemplary_sufficient_points - 1
+        self.exemplary_country_code = CountryCode.DE
+        self.exemplary_user = UserFactory.create(self.exemplary_country_code)
+        self.exemplary_sufficient_points = (
+            self.exemplary_user.redemption_strategy.required_points
+        )
+        self.exemplary_insufficient_points = Points(
+            self.exemplary_sufficient_points - 1
+        )
 
     def test_logs_user_redemption(self) -> None:
         with self.assertLogs(logger=logger) as log:
-            self.redemption_service.redeem(
-                self.exemplary_user, self.exemplary_sufficient_points
-            )
+            self.exemplary_user.points = self.exemplary_sufficient_points
+            self.redemption_service.redeem(self.exemplary_user)
+            self.exemplary_user.points = self.exemplary_insufficient_points
+            self.redemption_service.redeem(self.exemplary_user)
         self.assertIn(
-            f"Redeemed {self.exemplary_sufficient_points}, for a {self.exemplary_user}",
+            f"Redeemed {self.exemplary_sufficient_points} points, "
+            f"for user {self.exemplary_user.id}",
             log.output[0],
         )
-        # todo: self.assertIn inssuficient amount of points case
-
-    def test_uses_payment_gateway_to_transfer_money_to_user(self) -> None:
-        user = UserFactory.create()
-        amount = Decimal(100)
-        self.redemption_service.redeem(user, amount)
-        self.payment_gateway.assert_called_once_with(user.account_no, amount)
-
-    @unittest.skip("Not implemented yet")
-    def test_strategy_bank_transfer_using_payment_gateway(self) -> None:
-        user = UserFactory.create()  # country that uses bank transfer
-        sufficient_amount = self.exemplary_sufficient_points
-        self.redemption_service.redeem(user, sufficient_amount)
-        self.payment_gateway.transfer.assert_called_once_with(
-            user.account_no, sufficient_amount
+        self.assertIn(
+            f"Not enough points, got {self.exemplary_user.points} "
+            f"out of {self.exemplary_sufficient_points}",
+            log.output[1],
         )
 
-    @unittest.skip("Not implemented yet")
+    def test_strategy_bank_transfer_using_payment_gateway(self) -> None:
+        user = UserFactory.create(CountryCode.DE)  # country that uses bank transfer
+        user.points = user.redemption_strategy.required_points
+        self.redemption_service.redeem(user)
+        assert isinstance(user.redemption_strategy, BankTransfer)
+        self.payment_gateway.transfer.assert_called_once_with(
+            user.account_no, user.redemption_strategy.award_amount
+        )
+
     def test_strategy_gift_card_not_using_payment_gateway(self) -> None:
-        user = UserFactory.create()  # country that uses gift card
-        sufficient_amount = self.exemplary_insufficient_points
-        self.redemption_service.redeem(user, sufficient_amount)
+        user = UserFactory.create(CountryCode.FR)  # country that uses gift card
+        user.points = user.redemption_strategy.required_points
+        self.redemption_service.redeem(user)
         self.payment_gateway.transfer.assert_not_called()
 
 
 class TestPaymentGateway(TestCase):
     def test_has_transfer_method(self) -> None:
-        account_no = UserFactory.create().account_no
-        with self.assertLogs(logger) as log:
-            PaymentGateway().transfer(account_no, Decimal(100))
+        account_no = UserFactory.create(CountryCode.DE).account_no
+        with self.assertLogs(logger=logger) as log:
+            PaymentGateway().transfer(account_no, Points(Decimal(100)))
         self.assertIn(
             f"Successfully transferred 100 to {account_no}",
             log.output[0],
